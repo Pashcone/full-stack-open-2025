2d78db50cda099daa658a3ae67f9697c70ace0fa
Pashcone <pascatudor@gmail.com>
2025-08-07 19:10:35 +0300

    ss

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -47,9 +47,9 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
