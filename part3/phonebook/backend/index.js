d0db060e4bf785a210a0991f1dd068626f131cee
Pashcone <pascatudor@gmail.com>
2025-08-08 14:11:55 +0300

    cs

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,13 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(express.static("./"));
 
 let persons = [
   {
@@ -40,10 +34,9 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const dir = path.join(__dirname, "./"); // or 'public'
-
 app.use(express.json());
 app.use(requestLogger);
+app.use(express.static("dist"));
 
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
@@ -53,31 +46,9 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
-
-  console.log("|||||||||||||||||||||||||||||||||||||||||||||||||");
-
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
-    console.log("Directory entries:", files);
-  });
-
-  fs.readdir(dir, { withFileTypes: true }, (err, entries) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
-    entries.forEach((entry) => {
-      console.log(
-        `${entry.name} — ${entry.isDirectory() ? "directory" : "file"}`
-      );
-    });
-  });
-  
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -129,7 +100,7 @@ app.post("/api/persons", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   console.log("delete id", id);
-  const deletedPerson = persons.find((person) => person.id === id);
+  const deletedPerson = persons.find((person) => person.id === id)
   if (!deletedPerson)
     return response.status(404).json({
       error: "person not found",
@@ -137,7 +108,7 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
   response.status(204).json({
     message: `Person with id ${deletedPerson.id} deleted successfully`,
-  });
+  })
 });
 
 app.get("/info", (request, response) => {
@@ -148,7 +119,7 @@ app.get("/info", (request, response) => {
   );
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
